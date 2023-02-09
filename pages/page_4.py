4421c591749f56828c6d579bca93e2cbb73fae9d
sorujko <116210481+sorujko@users.noreply.github.com>
2023-02-09 20:36:10 +0100

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -239,6 +239,7 @@ with tab2:
     @st.experimental_singleton
     def installff():
         os.system('sbase install geckodriver')
+        os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
     _ = installff()
     import pandas as pd
