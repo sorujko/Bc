791a8c9a9cb201a84ac3197ae1e6fc81398bb69e
Juraj Hakoš <116210481+sorujko@users.noreply.github.com>
2023-02-28 23:31:50 +0100

    Update 🌲Random walk.py

--- pages/🌲Random walk.py
+++ pages/🌲Random walk.py
@@ -104,30 +104,5 @@ ax2.scatter(rw.x_values[-1],rw.y_values[-1],s=100 , c='red')
 
 st.pyplot(fig)
 
-import matplotlib.pyplot as plt
-import numpy as np
-import streamlit as st
-import time
 
-fig, ax1 = plt.subplots()
-
-max_x = 5
-max_rand = 10
-
-x = np.arange(0, max_x)
-ax1.set(xlim=(-(strana_lesa+15), (strana_lesa+15)), ylim=(-(strana_lesa+15), (strana_lesa+15)))
-line, = ax1.plot(rw.x_values,rw.y_values , marker='o', markerfacecolor="green" )
-the_plot = st.pyplot(plt)
-
-def init():  # give a clean slate to start
-    line.set_ydata([np.nan] * len(x))
-
-def animate(i):  # update the y values (every 1000ms)
-    line.set_ydata(np.random.randint(0, max_rand, max_x))
-    the_plot.pyplot(plt)
-
-init()
-for i in range(100):
-    animate(i)
-    time.sleep(0.1)
 
