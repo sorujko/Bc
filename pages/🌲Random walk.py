a54605814434a949b0cc67ecddc6a024023e09e7
Juraj Hakoš <116210481+sorujko@users.noreply.github.com>
2023-03-01 17:11:38 +0100

    video

--- pages/🌲Random walk.py
+++ pages/🌲Random walk.py
@@ -106,41 +106,46 @@ st.pyplot(fig)
 
 #pridat tu spravu o tom ci sa dostali z lesa alebo nie
 
+if rw.vysledok=='STRATILI STE SA V LESE!!':
+    video_file = open('videos/gif.mp4', 'rb')
+    video_bytes = video_file.read()
 
-import time
+    st.video(video_bytes , start_time=0 )
+    
+else:
+    import time
 
-fig, ax1 = plt.subplots()
-
-max_x = 5
-max_rand = 10
-
-ax1.plot(x1, y1, marker = 'o')
-ax1.plot(x2, y2, marker = 'o')
-ax1.plot(x3, y3, marker = 'o')
-ax1.plot(x4, y4, marker = 'o')
-ax1.set(xlim=(-(strana_lesa+15), (strana_lesa+15)), ylim=(-(strana_lesa+15), (strana_lesa+15)))
+    fig, ax1 = plt.subplots()
 
 
-#line, = ax1.plot(rw.x_values,rw.y_values , marker='o', markerfacecolor="green" )
-the_plot = st.pyplot(fig)
 
-#def init():  # give a clean slate to start
-#    line.set_ydata([np.nan] * len(x))
-
-def animate(i):  # update the y values (every 1000ms)
-    try:
-        line=ax1.plot(rw.x_values[i:i+2],rw.y_values[i:i+2] , marker='o', markerfacecolor="green"  )
-        
-        ax1.text(rw.x_values[i]+0.5, rw.y_values[i]+0.05, str(i), fontsize=8, color='black')
-        
-        the_plot.pyplot(fig)
-    except:
-        pass
+    ax1.plot(x1, y1, marker = 'o')
+    ax1.plot(x2, y2, marker = 'o')
+    ax1.plot(x3, y3, marker = 'o')
+    ax1.plot(x4, y4, marker = 'o')
+    ax1.set(xlim=(-(strana_lesa+15), (strana_lesa+15)), ylim=(-(strana_lesa+15), (strana_lesa+15)))
 
 
-for i in range(len(point_numbers)):
-    animate(i)
-    time.sleep(0.6)
+    #line, = ax1.plot(rw.x_values,rw.y_values , marker='o', markerfacecolor="green" )
+    the_plot = st.pyplot(fig)
+
+    #def init():  # give a clean slate to start
+    #    line.set_ydata([np.nan] * len(x))
+
+    def animate(i):  # update the y values (every 1000ms)
+        try:
+            line=ax1.plot(rw.x_values[i:i+2],rw.y_values[i:i+2] , marker='o', markerfacecolor="green"  )
+            
+            ax1.text(rw.x_values[i]+0.5, rw.y_values[i]+0.05, str(i), fontsize=8, color='black')
+            
+            the_plot.pyplot(fig)
+        except:
+            pass
+
+
+    for i in range(len(point_numbers)):
+        animate(i)
+        time.sleep(0.6)
     
 
 
